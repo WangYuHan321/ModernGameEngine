0cfc4e4d2abc09b87328cdb8b82b50f6bb0045d2
WangYuHan <1269940762@qq.com>
2025-09-27 22:15:04 +0800

    [modify] add util for vulkan renderer .

--- Unit_Test/draw_render_base/include/UtilVulkan.h
+++ Unit_Test/draw_render_base/include/UtilVulkan.h
@@ -126,7 +126,7 @@ namespace vkRenderer
 
 	void CHECK(bool check, const char* fileName, int inlineNumber);
 
-	//bool SetupDebugCallbacks(VkInstance instance, VkDebugUtilsMessengerEXT* messenger, VkDebugReportCallbackEXT* reportCallback);
+	bool SetupDebugCallbacks(VkInstance instance, VkDebugUtilsMessengerEXT* messenger, VkDebugReportCallbackEXT* reportCallback);
 
 	VkShaderStageFlagBits GlslangShaderStageToVulkan(glslang_stage_t sh);
 
@@ -160,9 +160,88 @@ namespace vkRenderer
 		};
 	}
 
+	void CreateInstance(VkInstance* instance);
+	
+	VkResult CreateDevice(VkPhysicalDevice physicalDevice, VkPhysicalDeviceFeatures devFeature, uint32_t graphicsFamily, VkDevice* device);
 
+	VkResult CreateSwapchain(VkDevice device, VkPhysicalDevice physicalDevice, VkSurfaceKHR surface, uint32_t graphicsFamily, uint32_t width, uint32_t height, VkSwapchainKHR* swapchain, bool supportScreenshots = false);
 
+	size_t CreateSwapchainImages(VkDevice device, VkSwapchainKHR swapchain, std::vector<VkImage>& swapchainImages, std::vector<VkImageView>& swapchainImageViews);
 
+	VkResult CreateSemaphore(VkDevice device, VkSemaphore* outSemaphore);
+
+	bool CreateTextureSampler(VkDevice device, VkSampler* sampler, VkFilter minFilter = VK_FILTER_LINEAR, VkFilter maxFilter = VK_FILTER_LINEAR, VkSamplerAddressMode addressMode = VK_SAMPLER_ADDRESS_MODE_REPEAT);
+
+	bool CreateDescriptorPool(VulkanRenderDevice& vkDev, uint32_t uniformBufferCount, uint32_t storageBufferCount, uint32_t samplerCount, VkDescriptorPool* descriptorPool);
+
+	bool IsDeviceSuitable(VkPhysicalDevice device);
+
+	SwapchainSupportDetails QuerySwapchainSupport(VkPhysicalDevice device, VkSurfaceKHR surface);
+
+	VkSurfaceFormatKHR ChooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
+
+	VkPresentModeKHR ChooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
+
+	uint32_t ChooseSwapImageCount(const VkSurfaceCapabilitiesKHR& capabilities);
+
+	VkResult FindSuitablePhysicalDevice(VkInstance instance, std::function<bool(VkPhysicalDevice)> selector, VkPhysicalDevice* physicalDevice);
+
+	uint32_t FindQueueFamilies(VkPhysicalDevice device, VkQueueFlags desiredFlags);
+
+	VkFormat FindSupportedFormat(VkPhysicalDevice device, const std::vector<VkFormat>& candidates, VkImageTiling tiling, VkFormatFeatureFlags features);
+
+	uint32_t FindMemoryType(VkPhysicalDevice device, uint32_t typeFilter, VkMemoryPropertyFlags properties);
+
+	VkFormat FindDepthFormat(VkPhysicalDevice device);
+
+	bool HasStencilComponent(VkFormat format);
+
+	bool CreateGraphicsPipeline(
+		VulkanRenderDevice& vkDev,
+		VkRenderPass renderPass, VkPipelineLayout pipelineLayout,
+		const std::vector<const char*>& shaderFiles,
+		VkPipeline* pipeline,
+		VkPrimitiveTopology topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST /* defaults to triangles*/,
+		bool useDepth = true,
+		bool useBlending = true,
+		bool dynamicScissorState = false,
+		int32_t customWidth = -1,
+		int32_t customHeight = -1,
+		uint32_t numPatchControlPoints = 0);
+
+	VkResult CreateComputePipeline(VkDevice device, VkShaderModule computeShader, VkPipelineLayout pipelineLayout, VkPipeline* pipeline);
+
+	bool CreateSharedBuffer(VulkanRenderDevice& vkDev, VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory);
+
+	bool CreateBuffer(VkDevice device, VkPhysicalDevice physicalDevice, VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory);
+	bool CreateImage(VkDevice device, VkPhysicalDevice physicalDevice, uint32_t width, uint32_t height, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory, VkImageCreateFlags flags = 0, uint32_t mipLevels = 1);
+
+	bool CreateVolume(VkDevice device, VkPhysicalDevice physicalDevice, uint32_t width, uint32_t height, uint32_t depth,
+		VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory, VkImageCreateFlags flags);
+
+	bool CreateOffscreenImage(VulkanRenderDevice& vkDev,
+		VkImage& textureImage, VkDeviceMemory& textureImageMemory,
+		uint32_t texWidth, uint32_t texHeight,
+		VkFormat texFormat,
+		uint32_t layerCount, VkImageCreateFlags flags);
+
+	bool CreateOffscreenImageFromData(VulkanRenderDevice& vkDev,
+		VkImage& textureImage, VkDeviceMemory& textureImageMemory,
+		void* imageData, uint32_t texWidth, uint32_t texHeight,
+		VkFormat texFormat,
+		uint32_t layerCount, VkImageCreateFlags flags);
+
+	bool CreateDepthSampler(VkDevice device, VkSampler* sampler);
+
+	bool CreateUniformBuffer(VulkanRenderDevice& vkDev, VkBuffer& buffer, VkDeviceMemory& bufferMemory, VkDeviceSize bufferSize);
+
+	/** Copy [data] to GPU device buffer */
+	void UploadBufferData(VulkanRenderDevice& vkDev, const VkDeviceMemory& bufferMemory, VkDeviceSize deviceOffset, const void* data, const size_t dataSize);
+
+	/** Copy GPU device buffer data to [outData] */
+	void DownloadBufferData(VulkanRenderDevice& vkDev, const VkDeviceMemory& bufferMemory, VkDeviceSize deviceOffset, void* outData, size_t dataSize);
+
+	bool CreateImageView(VkDevice device, VkImage image, VkFormat format, VkImageAspectFlags aspectFlags, VkImageView* imageView, VkImageViewType viewType = VK_IMAGE_VIEW_TYPE_2D, uint32_t layerCount = 1, uint32_t mipLevels = 1);
 }
 
 
--- Unit_Test/draw_render_base/src/UtilVulkan.cpp
+++ Unit_Test/draw_render_base/src/UtilVulkan.cpp
@@ -114,7 +114,7 @@ void vkRenderer::CHECK(bool check, const char* fileName, int inlineNumber)
 		exit(EXIT_FAILURE);
 	}
 }
-/*
+
 bool vkRenderer::SetupDebugCallbacks(VkInstance instance, VkDebugUtilsMessengerEXT* messenger, VkDebugReportCallbackEXT* reportCallback)
 {
 	{
@@ -153,7 +153,7 @@ bool vkRenderer::SetupDebugCallbacks(VkInstance instance, VkDebugUtilsMessengerE
 
 }
 
-*/
+
 
 VkShaderStageFlagBits vkRenderer::GlslangShaderStageToVulkan(glslang_stage_t sh)
 {
@@ -222,5 +222,448 @@ VkResult vkRenderer::CreateShaderModule(VkDevice device, ShaderModule* shader, c
 	return vkCreateShaderModule(device, &createInfo, nullptr, &shader->shaderModule);
 }
 
+void vkRenderer::CreateInstance(VkInstance* instance)
+{
+	const std::vector<const char*> validationLayers = {
+			"VK_LAYER_KHRONOS_validation"
+	};
+
+	const std::vector<const char*> exts = {
+#if defined(WIN32)
+	"VK_KHR_win32_surface"
+#endif
+#if defined(_APPLE_)
+	"VK_MVK_macos_surface"
+#endif
+#if defined (__linux__)
+	"VK_KHR_xcb_surface"
+#endif
+	, VK_EXT_DEBUG_UTILS_EXTENSION_NAME
+	, VK_EXT_DEBUG_REPORT_EXTENSION_NAME
+	, VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME
+	};
+
+	const VkApplicationInfo appInfo = {
+		.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+		.pNext = nullptr,
+		.pApplicationName = "Vulkan",
+		.applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+		.pEngineName = "No Engine",
+		.engineVersion = VK_MAKE_VERSION(1, 0, 0),
+		.apiVersion = VK_API_VERSION_1_1
+	};
+
+	const VkInstanceCreateInfo createInfo =
+	{
+		.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+		.pNext = nullptr,
+		.flags = 0,
+		.pApplicationInfo = &appInfo,
+		.enabledLayerCount = static_cast<uint32_t>(validationLayers.size()),
+		.ppEnabledLayerNames = validationLayers.data(),
+		.enabledExtensionCount = static_cast<uint32_t>(exts.size()),
+		.ppEnabledExtensionNames = exts.data()
+	};
+
+	VK_CHECK(vkCreateInstance(&createInfo, nullptr, instance));
+
+	//volkLoadInstance(*instance);
+}
+
+VkResult vkRenderer::CreateDevice(VkPhysicalDevice physicalDevice, VkPhysicalDeviceFeatures devFeature, uint32_t graphicsFamily, VkDevice* device)
+{
+	const std::vector<const char*> extensions = {
+		VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+		VK_KHR_SHADER_DRAW_PARAMETERS_EXTENSION_NAME
+	};
+
+	const float queuePriority = 1.0f;
+
+	const VkDeviceQueueCreateInfo qci = {
+		.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+		.pNext = nullptr,
+		.flags = 0,
+		.queueFamilyIndex = graphicsFamily,
+		.queueCount = 1,
+		.pQueuePriorities = &queuePriority
+	};
+	const VkDeviceCreateInfo ci = {
+				.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+		.pNext = nullptr,
+		.flags = 0,
+		.queueCreateInfoCount = 1,
+		.pQueueCreateInfos = &qci,
+		.enabledLayerCount = 0,
+		.ppEnabledLayerNames = nullptr,
+		.enabledExtensionCount = static_cast<uint32_t>(extensions.size()),
+		.ppEnabledExtensionNames = extensions.data(),
+		.pEnabledFeatures = &devFeature
+	};
+
+	return vkCreateDevice(physicalDevice, &ci, nullptr, device);
+}
+
+VkResult vkRenderer::CreateSwapchain(VkDevice device, VkPhysicalDevice physicalDevice, VkSurfaceKHR surface, uint32_t graphicsFamily, uint32_t width, uint32_t height, VkSwapchainKHR* swapchain, bool supportScreenshots)
+{
+	auto swapchainSupport = QuerySwapchainSupport(physicalDevice, surface);
+	auto surfaceFormat = ChooseSwapSurfaceFormat(swapchainSupport.formats);
+	auto presentMode = ChooseSwapPresentMode(swapchainSupport.presentModes);
+
+	const VkSwapchainCreateInfoKHR ci =
+	{
+		.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
+		.flags = 0,
+		.minImageCount = ChooseSwapImageCount(swapchainSupport.capabilities),
+		.imageFormat = surfaceFormat.format,
+		.imageColorSpace = surfaceFormat.colorSpace,
+		.imageExtent = {.width = width, .height = height},
+		.imageArrayLayers = 1,
+		.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | ((supportScreenshots) ? VK_IMAGE_USAGE_TRANSFER_SRC_BIT : 0u),
+		.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE,
+		.queueFamilyIndexCount = 1,
+		.pQueueFamilyIndices = &graphicsFamily,
+		.preTransform = swapchainSupport.capabilities.currentTransform,
+		.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR,
+		.presentMode = presentMode,
+		.clipped = VK_TRUE,
+		.oldSwapchain = VK_NULL_HANDLE
+	};
+
+	return vkCreateSwapchainKHR(device, &ci, nullptr, swapchain);
+}
+
+size_t vkRenderer::CreateSwapchainImages(VkDevice device, 
+	VkSwapchainKHR swapchain, std::vector<VkImage>& swapchainImages, std::vector<VkImageView>& swapchainImageViews)
+{
+	uint32_t imageCount = 0;
+	VK_CHECK(vkGetSwapchainImagesKHR(device, swapchain, &imageCount, nullptr));
+
+	swapchainImages.resize(imageCount);
+	swapchainImageViews.resize(imageCount);
+
+	VK_CHECK(vkGetSwapchainImagesKHR(device, swapchain, &imageCount, swapchainImages.data()));
+
+	for (unsigned int i = 0; i < imageCount; i++)
+	{
+		if (!CreateImageView(device, swapchainImages[i], VK_FORMAT_B8G8R8A8_UNORM, VK_IMAGE_ASPECT_COLOR_BIT, &swapchainImageViews[i]))
+			exit(0);
+	}
+
+	return static_cast<size_t>(imageCount);
+}
+
+VkResult vkRenderer::CreateSemaphore(VkDevice device, VkSemaphore* outSemaphore)
+{
+	const VkSemaphoreCreateInfo ci =
+	{
+		.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO
+	};
+
+	return vkCreateSemaphore(device, &ci, nullptr, outSemaphore);
+}
+
+bool vkRenderer::CreateTextureSampler(VkDevice device, VkSampler* sampler, VkFilter minFilter, VkFilter maxFilter, VkSamplerAddressMode addressMode)
+{
+	const VkSamplerCreateInfo samplerInfo =
+	{
+		.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO,
+		.pNext = nullptr,
+		.flags = 0,
+		.magFilter = VK_FILTER_LINEAR,
+		.minFilter = VK_FILTER_LINEAR,
+		.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR,
+		.addressModeU = addressMode,
+		.addressModeV = addressMode,
+		.addressModeW = addressMode,
+		.mipLodBias = 0.0f,
+		.anisotropyEnable = VK_FALSE,
+		.maxAnisotropy = 1,
+		.compareEnable = VK_FALSE,
+		.compareOp = VK_COMPARE_OP_ALWAYS,
+		.minLod = 0.0f,
+		.maxLod = 0.0f,
+		.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK,
+		.unnormalizedCoordinates = VK_FALSE
+	};
+
+	return (vkCreateSampler(device, &samplerInfo, nullptr, sampler) == VK_SUCCESS);
+}
+
+bool vkRenderer::CreateDescriptorPool(VulkanRenderDevice& vkDev, uint32_t uniformBufferCount, uint32_t storageBufferCount, uint32_t samplerCount, VkDescriptorPool* descriptorPool)
+{
+	const uint32_t imageCount = static_cast<uint32_t>(vkDev.swapchainImages.size());
+
+	std::vector<VkDescriptorPoolSize> poolSize;
+
+	if (uniformBufferCount)
+		poolSize.push_back(VkDescriptorPoolSize{ .type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, .descriptorCount = imageCount * uniformBufferCount });
+
+	if (storageBufferCount)
+		poolSize.push_back(VkDescriptorPoolSize{ .type = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, .descriptorCount = imageCount * storageBufferCount });
+
+	if (samplerCount)
+		poolSize.push_back(VkDescriptorPoolSize{ .type = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, .descriptorCount = imageCount * samplerCount });
+
+	const VkDescriptorPoolCreateInfo poolInfo = {
+	.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO,
+	.pNext = nullptr,
+	.flags = 0,
+	.maxSets = static_cast<uint32_t>(imageCount),
+	.poolSizeCount = static_cast<uint32_t>(poolSize.size()),
+	.pPoolSizes = poolSize.empty() ? nullptr : poolSize.data()
+	};
+
+	return (vkCreateDescriptorPool(vkDev.device, &poolInfo, nullptr, descriptorPool) == VK_SUCCESS);
+}
+
+bool vkRenderer::IsDeviceSuitable(VkPhysicalDevice device)
+{
+	VkPhysicalDeviceProperties deviceProperties;
+	vkGetPhysicalDeviceProperties(device, &deviceProperties);
+
+	VkPhysicalDeviceFeatures deviceFeatures;
+	vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+
+	const bool isDiscreteGPU = deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU;
+	const bool isIntegratedGPU = deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU;
+	const bool isGPU = isDiscreteGPU || isIntegratedGPU;
+
+	return isGPU && deviceFeatures.geometryShader;
+}
+
+vkRenderer::SwapchainSupportDetails vkRenderer::QuerySwapchainSupport(VkPhysicalDevice device, VkSurfaceKHR surface)
+{
+	SwapchainSupportDetails details;
+	vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, surface, &details.capabilities);
+
+	uint32_t formatCount;
+	vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, nullptr);
+
+	if (formatCount)
+	{
+		details.formats.resize(formatCount);
+		vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, details.formats.data());
+	}
+
+	uint32_t presentModeCount;
+	vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, nullptr);
+
+	if (presentModeCount)
+	{
+		details.presentModes.resize(presentModeCount);
+		vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, details.presentModes.data());
+	}
+
+	return details;
+}
+
+VkSurfaceFormatKHR vkRenderer::ChooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats)
+{
+	return { VK_FORMAT_B8G8R8A8_UNORM, VK_COLOR_SPACE_SRGB_NONLINEAR_KHR };
+}
+
+VkPresentModeKHR vkRenderer::ChooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes)
+{
+	for (const auto mode : availablePresentModes)
+		if (mode == VK_PRESENT_MODE_MAILBOX_KHR)
+			return mode;
+
+	// FIFO will always be supported
+	return VK_PRESENT_MODE_FIFO_KHR;
+}
+
+uint32_t vkRenderer::ChooseSwapImageCount(const VkSurfaceCapabilitiesKHR& capabilities)
+{
+	const uint32_t imageCount = capabilities.minImageCount + 1;
+
+	const bool imageCountExceeded = capabilities.maxImageCount > 0 && imageCount > capabilities.maxImageCount;
+
+	return imageCountExceeded ? capabilities.maxImageCount : imageCount;
+}
+
+VkResult vkRenderer::FindSuitablePhysicalDevice(VkInstance instance, std::function<bool(VkPhysicalDevice)> selector, VkPhysicalDevice* physicalDevice)
+{
+	uint32_t deviceCount = 0;
+	VK_CHECK_RET(vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr));
+
+	if (!deviceCount) return VK_ERROR_INITIALIZATION_FAILED;
+
+	std::vector<VkPhysicalDevice> devices(deviceCount);
+	VK_CHECK_RET(vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data()));
+
+	for (const auto& device : devices)
+	{
+		if (selector(device))
+		{
+			*physicalDevice = device;
+			return VK_SUCCESS;
+		}
+	}
+
+	return VK_ERROR_INITIALIZATION_FAILED;
+}
+
+uint32_t vkRenderer::FindQueueFamilies(VkPhysicalDevice device, VkQueueFlags desiredFlags)
+{
+	uint32_t familyCount = 0;
+	vkGetPhysicalDeviceQueueFamilyProperties(device, &familyCount, nullptr);
+
+	std::vector<VkQueueFamilyProperties> families(familyCount);
+	vkGetPhysicalDeviceQueueFamilyProperties(device, &familyCount, families.data());
+
+	for (uint32_t i = 0; i != families.size(); i++)
+		if (families[i].queueCount > 0 && families[i].queueFlags & desiredFlags)
+			return i;
+
+	return 0;
+}
+
+VkFormat vkRenderer::FindSupportedFormat(VkPhysicalDevice device, const std::vector<VkFormat>& candidates, VkImageTiling tiling, VkFormatFeatureFlags features)
+{
+	for (VkFormat format : candidates) {
+		VkFormatProperties props;
+		vkGetPhysicalDeviceFormatProperties(device, format, &props);
+
+		if (tiling == VK_IMAGE_TILING_LINEAR && (props.linearTilingFeatures & features) == features) {
+			return format;
+		}
+		else if (tiling == VK_IMAGE_TILING_OPTIMAL && (props.optimalTilingFeatures & features) == features) {
+			return format;
+		}
+	}
+
+	printf("failed to find supported format!\n");
+	exit(0);
+}
+
+uint32_t vkRenderer::FindMemoryType(VkPhysicalDevice device, uint32_t typeFilter, VkMemoryPropertyFlags properties)
+{
+	VkPhysicalDeviceMemoryProperties memProperties;
+	vkGetPhysicalDeviceMemoryProperties(device, &memProperties);
+
+	for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+		if ((typeFilter & (1 << i)) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {
+			return i;
+		}
+	}
+
+	return 0xFFFFFFFF;
+}
+
+VkFormat vkRenderer::FindDepthFormat(VkPhysicalDevice device)
+{
+	return FindSupportedFormat(
+		device,
+		{ VK_FORMAT_D32_SFLOAT, VK_FORMAT_D32_SFLOAT_S8_UINT, VK_FORMAT_D24_UNORM_S8_UINT },
+		VK_IMAGE_TILING_OPTIMAL,
+		VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT
+	);
+}
+
+bool vkRenderer::HasStencilComponent(VkFormat format)
+{
+	return true;
+}
+
+bool vkRenderer::CreateGraphicsPipeline(
+	VulkanRenderDevice& vkDev,
+	VkRenderPass renderPass, VkPipelineLayout pipelineLayout,
+	const std::vector<const char*>& shaderFiles,
+	VkPipeline* pipeline,
+	VkPrimitiveTopology topology/* defaults to triangles*/,
+	bool useDepth ,
+	bool useBlending ,
+	bool dynamicScissorState ,
+	int32_t customWidth,
+	int32_t customHeight,
+	uint32_t numPatchControlPoints)
+{
+	return true;
+}
+
+VkResult vkRenderer::CreateComputePipeline(VkDevice device, VkShaderModule computeShader, VkPipelineLayout pipelineLayout, VkPipeline* pipeline)
+{
+	return VK_SUCCESS;
+}
+
+bool vkRenderer::CreateSharedBuffer(VulkanRenderDevice& vkDev, VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory)
+{
+	return true;
+}
+
+bool vkRenderer::CreateBuffer(VkDevice device, VkPhysicalDevice physicalDevice, VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory)
+{
+	return true;
+}
+
+bool vkRenderer::CreateImage(VkDevice device, VkPhysicalDevice physicalDevice, uint32_t width, uint32_t height, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory, VkImageCreateFlags flags, uint32_t mipLevels)
+{
+	return true;
+}
+
+bool vkRenderer::CreateVolume(VkDevice device, VkPhysicalDevice physicalDevice, uint32_t width, uint32_t height, uint32_t depth,
+	VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory, VkImageCreateFlags flags)
+{
+	return true;
+}
+
+bool vkRenderer::CreateOffscreenImage(VulkanRenderDevice& vkDev,
+	VkImage& textureImage, VkDeviceMemory& textureImageMemory,
+	uint32_t texWidth, uint32_t texHeight,
+	VkFormat texFormat,
+	uint32_t layerCount, VkImageCreateFlags flags);
+
+bool vkRenderer::CreateOffscreenImageFromData(VulkanRenderDevice& vkDev,
+	VkImage& textureImage, VkDeviceMemory& textureImageMemory,
+	void* imageData, uint32_t texWidth, uint32_t texHeight,
+	VkFormat texFormat,
+	uint32_t layerCount, VkImageCreateFlags flags)
+{
+	return true;
+}
+
+bool vkRenderer::CreateDepthSampler(VkDevice device, VkSampler* sampler)
+{
+	return true;
+}
+
+bool vkRenderer::CreateUniformBuffer(VulkanRenderDevice& vkDev, VkBuffer& buffer, VkDeviceMemory& bufferMemory, VkDeviceSize bufferSize)
+{
+	return true;
+}
+
+/** Copy [data] to GPU device buffer */
+void vkRenderer::UploadBufferData(VulkanRenderDevice& vkDev, const VkDeviceMemory& bufferMemory, VkDeviceSize deviceOffset, const void* data, const size_t dataSize)
+{
+
+}
+
+/** Copy GPU device buffer data to [outData] */
+void vkRenderer::DownloadBufferData(VulkanRenderDevice& vkDev, const VkDeviceMemory& bufferMemory, VkDeviceSize deviceOffset, void* outData, size_t dataSize)
+{
+
+}
+
+bool vkRenderer::CreateImageView(VkDevice device, VkImage image, VkFormat format, VkImageAspectFlags aspectFlags, VkImageView* imageView, VkImageViewType viewType , uint32_t layerCount, uint32_t mipLevels)
+{
+	const VkImageViewCreateInfo viewInfo =
+	{
+		.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+		.pNext = nullptr,
+		.flags = 0,
+		.image = image,
+		.viewType = viewType,
+		.format = format,
+		.subresourceRange =
+		{
+			.aspectMask = aspectFlags,
+			.baseMipLevel = 0,
+			.levelCount = mipLevels,
+			.baseArrayLayer = 0,
+			.layerCount = layerCount
+		}
+	};
+	return (vkCreateImageView(device, &viewInfo, nullptr, imageView) == VK_SUCCESS);
+}
 
 
