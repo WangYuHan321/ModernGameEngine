88161ac7c8502fa37727f98f6c9d73e608482d78
wangyuhan <1269940762@qq.com>
2025-09-29 13:18:48 +0800

    [modify] add test code .

--- android/examples/draw_triangle/test.cpp
+++ android/examples/draw_triangle/test.cpp
@@ -1,6 +1,9 @@
 #include <android_native_app_glue.h>
+#include "VulkanAndroid.h"
 
 void android_main(android_app* state)																
-{																									
-													
+{
+   androidApp = state;
+   vks::android::getDeviceConfig();
+    __android_log_print(ANDROID_LOG_INFO, "vulkanandroid", "Loading libVulkan.so...\n");
 }
