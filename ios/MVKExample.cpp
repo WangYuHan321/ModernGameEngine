36aa35cedd54c361c50988209dd07870f22c3563
WangYuHan <1269940762@qq.com>
2025-10-02 01:54:45 +0800

    [add] add some files .

---
+++ ios/MVKExample.cpp
@@ -0,0 +1,66 @@
+/*
+ *  MVKExample.cpp
+ *
+ *  Copyright (c) 2016-2017 The Brenwill Workshop Ltd.
+ *  This code is licensed under the MIT license (MIT) (http://opensource.org/licenses/MIT)
+ */
+
+
+#include "MVKExample.h"
+
+void MVKExample::displayLinkOutputCb() {                        // SRS - expose
+
+}
+
+void MVKExample::keyPressed(uint32_t keyChar) {					// SRS - handle keyboard key presses only (e.g. Pause, Space, etc)
+	switch (keyChar)
+	{
+
+	}
+}
+
+void MVKExample::keyDown(uint32_t keyChar) {					// SRS - handle physical keyboard key down/up
+}
+
+void MVKExample::keyUp(uint32_t keyChar) {
+
+}
+
+void MVKExample::mouseDown(double x, double y) {
+
+}
+
+void MVKExample::mouseUp() {
+}
+
+void MVKExample::rightMouseDown(double x, double y) {
+
+}
+
+void MVKExample::rightMouseUp() {
+
+}
+
+void MVKExample::otherMouseDown(double x, double y) {
+}
+
+void MVKExample::otherMouseUp() {
+}
+
+void MVKExample::mouseDragged(double x, double y) {
+}
+
+void MVKExample::scrollWheel(short wheelDelta) {
+}
+
+void MVKExample::fullScreen(bool fullscreen) {
+
+}
+
+MVKExample::MVKExample(void* view, double scaleUI) {
+
+}
+
+MVKExample::~MVKExample() {
+
+}
