a169e9f681e7866f8d4431e55c2f7c400117d222
wangyuhan <1269940762@qq.com>
2025-12-15 16:50:20 +0800

    [add] add project cubemap .

---
+++ Unit_Test/draw_cubemap/include/ApplicationWindow.h
@@ -0,0 +1,43 @@
+﻿#include "Render/Vulkan/ApplicationBase.h"
+
+struct UniformData
+{
+
+};
+
+struct ProjectModel
+{
+	GlTFModel skyModel;
+	std::vector<GlTFModel> objects;
+};
+
+class ApplicationWin : public ApplicationBase
+{
+public:
+	ApplicationWin();
+	~ApplicationWin() override;
+	
+private:
+
+	ProjectModel m_projModel;
+	VulkanTextureCubeMap m_texCubeMap;
+
+	std::array<Render::Vulkan::Buffer, MAX_FRAMES_IN_FLIGHT> m_uniformBuffer;
+	std::array<VkDescriptorSet, MAX_FRAMES_IN_FLIGHT> m_descriptorSets{};
+public:
+	void DrawUI(const VkCommandBuffer cmdBuffer);
+	void SetupDescriptors();
+	void UpdateUniformBuffers();
+	
+	VkShaderModule LoadSPIRVShader(const std::string& filename);
+
+	void PreparePipeline();
+	void PrepareUniformBuffer();
+	void BuildCommandBuffer();
+
+	void Prepare() override;
+	void Render();
+
+	void LoadAsset();
+};
+
---
+++ Unit_Test/draw_cubemap/src/ApplicationWindow.cpp
@@ -0,0 +1,129 @@
+﻿#include "ApplicationWindow.h"
+#include <random>
+
+// plants.gltf 模型文件可能包含多个mesh
+// 每个mesh对应一种植物类型，与纹理数组层对应
+
+ApplicationWin::ApplicationWin():
+	ApplicationBase()
+{
+}
+
+ApplicationWin::~ApplicationWin()
+{
+	
+}
+
+void ApplicationWin::DrawUI(const VkCommandBuffer cmdBuffer)
+{
+	const VkViewport viewport = Render::Vulkan::Initializer::Viewport((float)width, (float)height, 0.0f, 1.0f);
+	const VkRect2D scissor = Render::Vulkan::Initializer::Rect2D(width, height, 0, 0);
+	vkCmdSetViewport(cmdBuffer, 0, 1, &viewport);
+	vkCmdSetScissor(cmdBuffer, 0, 1, &scissor);
+
+	ui.Draw(cmdBuffer);
+}
+
+void ApplicationWin::SetupDescriptors()
+{
+	
+}
+
+VkShaderModule ApplicationWin::LoadSPIRVShader(const std::string& filename)
+{
+	size_t shaderSize;
+	char* shaderCode{ nullptr };
+
+#if defined (__ANDROID__)
+    AAsset* asset = AAssetManager_open(androidApp->activity->assetManager,filename.c_str(), AASSET_MODE_STREAMING );
+    assert(asset);
+    shaderSize = AAsset_getLength(asset);
+    assert(shaderSize > 0);
+
+    shaderCode = new char[shaderSize];
+    AAsset_read(asset, shaderCode, shaderSize);
+    AAsset_close(asset);
+#else
+	std::ifstream is(filename, std::ios::binary | std::ios::in | std::ios::ate);
+
+	if (is.is_open())
+	{
+		shaderSize = is.tellg();
+		is.seekg(0, std::ios::beg);
+		// Copy file contents into a buffer
+		shaderCode = new char[shaderSize];
+		is.read(shaderCode, shaderSize);
+		is.close();
+		assert(shaderSize > 0);
+	}
+#endif
+	if (shaderCode)
+	{
+		// Create a new shader module that will be used for pipeline creation
+		VkShaderModuleCreateInfo shaderModuleCI{};
+		shaderModuleCI.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+		shaderModuleCI.codeSize = shaderSize;
+		shaderModuleCI.pCode = (uint32_t*)shaderCode;
+
+		VkShaderModule shaderModule;
+		VK_CHECK_RESULT(vkCreateShaderModule(m_device, &shaderModuleCI, nullptr, &shaderModule));
+
+		delete[] shaderCode;
+
+		return shaderModule;
+	}
+	else
+	{
+		std::cerr << "Error: Could not open shader file \"" << filename << "\"" << std::endl;
+		return VK_NULL_HANDLE;
+	}
+}
+
+void ApplicationWin::PreparePipeline()
+{
+	
+}
+
+void ApplicationWin::PrepareUniformBuffer()
+{
+}
+
+void ApplicationWin::BuildCommandBuffer()
+{
+}
+
+void ApplicationWin::Prepare() 
+{
+	ApplicationBase::Prepare();
+	LoadAsset(); // 加载图片
+	PrepareUniformBuffer();
+	SetupDescriptors();
+	PreparePipeline();
+	prepared = true;
+}
+
+void ApplicationWin::UpdateUniformBuffers()
+{
+}
+
+void ApplicationWin::Render()
+{
+	if (!prepared)
+		return;
+
+	VK_CHECK_RESULT(vkWaitForFences(m_device, 1, &m_waitFences[m_currentBuffer], VK_TRUE, UINT64_MAX));
+	VK_CHECK_RESULT(vkResetFences(m_device, 1, &m_waitFences[m_currentBuffer]));
+	m_swapChain.AcquireNextImage(m_presentCompleteSemaphores[m_currentBuffer], m_currentImageIndex);
+
+	UpdateUniformBuffers();
+	BuildCommandBuffer();
+
+	ApplicationBase::SubmitFrame(false);
+	
+	vkQueueWaitIdle(m_queue);
+}
+
+void ApplicationWin::LoadAsset()
+{
+
+}
