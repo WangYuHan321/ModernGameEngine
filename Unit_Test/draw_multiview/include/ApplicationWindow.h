47337d6d409e79482efd6559bfbff7866479a80e
wangyuhan <1269940762@qq.com>
2026-01-08 18:59:49 +0800

    [add] add project multivewpass project .

---
+++ Unit_Test/draw_multiview/include/ApplicationWindow.h
@@ -0,0 +1,60 @@
+﻿#include "Render/Vulkan/ApplicationBase.h"
+
+struct FrameBufferAttachment
+{
+	VkImage image{ VK_NULL_HANDLE };
+	VkDeviceMemory memory{ VK_NULL_HANDLE };
+	VkImageView view{ VK_NULL_HANDLE };
+};
+
+struct MultivewPass {
+	FrameBufferAttachment color;
+	FrameBufferAttachment depth;
+	VkFramebuffer frameBuffer{ VK_NULL_HANDLE };
+	VkRenderPass renderPass{ VK_NULL_HANDLE };
+	VkDescriptorImageInfo descriptor{ VK_NULL_HANDLE };
+	VkSampler sampler{ VK_NULL_HANDLE };
+};
+
+struct UniformData
+{
+	glm::mat4 projection[2];
+	glm::mat4 modelview[2];
+	glm::vec4 lightPos = glm::vec4(-2.5f, -3.5f, 0.0f, 1.0f);
+	float distortionAlpha = 0.2f;
+};
+
+class ApplicationWin : public ApplicationBase
+{
+public:
+
+
+	MultivewPass m_multivewPass;
+
+	GltfModel m_model;
+
+	UniformData m_uniformData;
+
+	std::array<Render::Vulkan::Buffer, MAX_FRAMES_IN_FLIGHT> m_uniformBuffers;
+
+	ApplicationWin();
+	~ApplicationWin() override;
+	
+private:
+
+	std::vector<std::string> m_filterName = { "sharpen" };
+
+public:
+	void DrawUI(const VkCommandBuffer cmdBuffer);
+	void CreateDescriptorPool();
+	void UpdateUniformBuffers();
+
+	void PrepareGraphicsPipeline();
+	void PrepareUniformBuffer();
+	void BuildGraphicsCommandBuffer();
+
+	void Prepare() override;
+	void Render();
+	void LoadAsset(std::string fileNamePath);
+};
+
