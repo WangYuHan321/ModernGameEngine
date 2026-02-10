36ad7d94cab2f235b4843ad8b65d1d99eafee03c
wangyuhan <1269940762@qq.com>
2026-02-10 14:50:00 +0800

    [add] add files VBuffer .

---
+++ FrameGraph/Vulkan/Buffer/VBuffer.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "../Public/FrameGraph.h"
+#include "vulkan/vulkan.h"
+
+namespace FrameGraph
+{
+	//
+	//Vulkan 立即Buffer
+	//
+	class VBuffer final
+	{
+		friend class VBufferUnitTest;
+
+		//类型
+	private:
+		using OnRelease_t = IFrameGraph::OnExternalBufferReleased_t;
+		using BufferViewMap_t = HashMap<BufferViewDesc, VkBufferView>;
+
+		//变量
+	private:
+		VkBuffer _buffer = VK_NULL_HANDLE;
+		MemoryID _memoryId;
+
+
+	};
+
+
+
+}
+
