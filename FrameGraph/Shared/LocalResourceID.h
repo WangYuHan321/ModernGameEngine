e7c3746dccb6e6f8a2f92eee1ce8e0a24cc69e5d
wangyuhan <1269940762@qq.com>
2026-02-11 16:17:07 +0800

    [modify] add shared local resource id .

--- FrameGraph/Shared/LocalResourceID.h
+++ FrameGraph/Shared/LocalResourceID.h
@@ -4,7 +4,20 @@
 #include <cstdint>
 #include <vector>
 
+#include "../Public/FrameGraph.h"
+
 namespace FrameGraph
 {
+	using RawMemoryID = FrameGraph::Local::ResourceID<15>;
+	using RawPipelineLayoutID = FrameGraph::Local::ResourceID<16>;
+	using RawRenderPassID = FrameGraph::Local::ResourceID<17>;
+	using RawFrameBufferID = FrameGraph::Local::ResourceID<18>;
+	using RawDescriptorPoolID = FrameGraph::Local::ResourceID<19>;
+
+	using MemoryID = FrameGraph::Local::ResourceIDWrap<RawMemoryID>;
+	using PipelineLayoutID = FrameGraph::Local::ResourceIDWrap<RawPipelineLayoutID>;
+	using RenderPassID = FrameGraph::Local::ResourceIDWrap<RawRenderPassID>;
+	using FrameBufferID = FrameGraph::Local::ResourceIDWrap<RawFrameBufferID>;
+	using DescriptorPoolID = FrameGraph::Local::ResourceIDWrap<RawDescriptorPoolID>;
 
 }
