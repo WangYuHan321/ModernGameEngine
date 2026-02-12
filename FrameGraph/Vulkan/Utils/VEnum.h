6f375e0b9f8c0199bcd7b1d9a8c3ebfdc28950ee
wangyuhan <1269940762@qq.com>
2026-02-12 10:11:12 +0800

    [add] add vulkan utils part .

---
+++ FrameGraph/Vulkan/Utils/VEnum.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "../Public/FrameGraph.h"
+#include "../Shared/LocalResourceID.h"
+#include "../STL/Common.h"
+#include "vulkan/vulkan.h"
+
+namespace FrameGraph
+{
+	enum class EQueueFamily : uint
+	{
+		_Count = 31,
+
+		External = VK_QUEUE_FAMILY_EXTERNAL,
+		Foreign = VK_QUEUE_FAMILY_FOREIGN_EXT,
+		Ignored = VK_QUEUE_FAMILY_IGNORED,
+		Unknown = Ignored,
+	};
+
+
+}
+
