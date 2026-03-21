e679b080531a6f9171d2ad7bc58c8b574d6a008f
wangyuhan <1269940762@qq.com>
2026-03-21 18:03:52 +0800

    [add] add zero operator .

--- FrameGraph/STL/CompileTime/Constants.h
+++ FrameGraph/STL/CompileTime/Constants.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include "../Common.h"
+#include "../Defines.h"
 
 namespace FrameGraph
 {
@@ -9,11 +10,72 @@ namespace FrameGraph
 		template<typename T>
 		GND constexpr operator const T()const
 		{
+			STATIC_ASSERT(T(~T(0)) > T(0));  // 确保是无符号类型
+			return T(~T(0));                    // 返回所有位都为1的值
 		}
 
+		template <typename T>
+		GND friend constexpr bool operator == (const T& left, const _UMax& right)
+		{
+			return T(right) == left;
+		}
+
+		template <typename T>
+		GND friend constexpr bool operator != (const T& left, const _UMax& right)
+		{
+			return T(right) != left;
+		}
 
 	};
 
 	static constexpr _UMax	UMax{};
+
+	struct _Zero
+	{
+		template<typename T>
+		GND constexpr operator const T()const
+		{
+			////STATIC_ASSERT( std::is_integral_v<T> || std::is_enum_v<T> );
+			return T(0);
+		}
+
+		template <typename T>
+		GND friend constexpr auto  operator == (const T& left, const _Zero& right)
+		{
+			return T(right) == left;
+		}
+
+		template <typename T>
+		GND friend constexpr auto  operator != (const T& left, const _Zero& right)
+		{
+			return T(right) != left;
+		}
+
+		template <typename T>
+		GND friend constexpr auto  operator > (const T& left, const _Zero& right)
+		{
+			return left > T(right);
+		}
+
+		template <typename T>
+		GND friend constexpr auto  operator < (const T& left, const _Zero& right)
+		{
+			return left < T(right);
+		}
+
+		template <typename T>
+		GND friend constexpr auto  operator >= (const T& left, const _Zero& right)
+		{
+			return left >= T(right);
+		}
+
+		template <typename T>
+		GND friend constexpr auto  operator <= (const T& left, const _Zero& right)
+		{
+			return left <= T(right);
+		}
+	};
+
+	static constexpr _Zero	Zero{};
 }
 
