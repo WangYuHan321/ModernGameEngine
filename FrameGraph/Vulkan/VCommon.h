16d4306339385f226be2a763dca3193239d7bd9e
wangyuhan <1269940762@qq.com>
2026-04-04 14:04:36 +0800

    [add]add vcommon.h

---
+++ FrameGraph/Vulkan/VCommon.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <assert.h>
+#include <cstdint>
+#include <vector>
+#include "vulkan/vulkan.h"
+#include "../Public/Config.h"
+#include "../STL/Containers/StaticString.h"
+#include "../STL/Containers/FixedArray.h"
+
+#include "../STL/Containers/Ptr.h"
+
+namespace FrameGraph
+{
+	using DebugName_t = StaticString<64>;
+
+	using VkDescriptorSets_t = FixedArray<VkDescriptorSet, GFG_MaxDescriptorSets>;
+
+
+}
