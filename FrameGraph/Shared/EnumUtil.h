c72e759d8fc70ed5fec079685a74b5b43061de50
wangyuhan <1269940762@qq.com>
2026-04-11 17:43:57 +0800

    [add] add EnumUtil.h

---
+++ FrameGraph/Shared/EnumUtil.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include <assert.h>
+#include <cstdint>
+#include <vector>
+
+#include "../STL/Math/Bytes.h"
+#include "../Public/VertexEnums.h"
+
+namespace FrameGraph
+{
+	GND inline BytesU EIndex_SizeOf(EIndex valude)
+	{
+		switch (valude)
+		{
+		case EIndex::UShort:	return SizeOf<uint16_t>;
+		case EIndex::UInt:		return SizeOf<uint32_t>;
+		case EIndex::Unknown:	break;
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+	
+}
