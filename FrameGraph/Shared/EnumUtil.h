100379dfaadf4a6dd5151e912af1dc7ff991edb8
wangyuhan <1269940762@qq.com>
2026-04-16 14:41:13 +0800

    [modify] modify shader stage .

--- FrameGraph/Shared/EnumUtil.h
+++ FrameGraph/Shared/EnumUtil.h
@@ -5,6 +5,8 @@
 #include <vector>
 
 #include "../STL/Math/Bytes.h"
+#include"../STL/CompileTime/Constants.h"
+#include "../Public/Config.h"
 #include "../Public/VertexEnums.h"
 #include "../Public/ShaderEnums.h"
 #include "../Public/EResourceState.h"
@@ -34,6 +36,37 @@ namespace FrameGraph
 		}
 	}
 
+	GND inline EResourceState  EResourceState_FromShaders(EShaderStage values)
+	{
+		EResourceState	result = Zero;
+
+		for (EShaderStage t = EShaderStage(1 << 0); t < EShaderStage::_Last; t = EShaderStage(uint(t) << 1))
+		{
+			switch (t)
+			{
+			case EShaderStage::Vertex:			result |= EResourceState::_VertexShader;			break;
+			case EShaderStage::TessControl:		result |= EResourceState::_TessControlShader;		break;
+			case EShaderStage::TessEvaluation:	result |= EResourceState::_TessEvaluationShader;	break;
+			case EShaderStage::Geometry:			result |= EResourceState::_GeometryShader;			break;
+			case EShaderStage::Fragment:			result |= EResourceState::_FragmentShader;			break;
+			case EShaderStage::Compute:			result |= EResourceState::_ComputeShader;			break;
+			case EShaderStage::MeshTask:			result |= EResourceState::_MeshTaskShader;			break;
+			case EShaderStage::Mesh:				result |= EResourceState::_MeshShader;				break;
+			case EShaderStage::RayGen:
+			case EShaderStage::RayAnyHit:
+			case EShaderStage::RayClosestHit:
+			case EShaderStage::RayMiss:
+			case EShaderStage::RayIntersection:
+			case EShaderStage::RayCallable:		result |= EResourceState::_RayTracingShader;		break;
+			case EShaderStage::_Last:
+			case EShaderStage::All:
+			case EShaderStage::AllGraphics:
+			case EShaderStage::AllRayTracing:
+			case EShaderStage::Unknown:
+			}
+		}
+		return result;
+	}
 
 	GND inline EResourceState  EResourceState_FromShaderAccess(EShaderAccess access)
 	{
