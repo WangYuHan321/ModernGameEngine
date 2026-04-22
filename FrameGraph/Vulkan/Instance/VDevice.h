0db29f5968d7dee1895a3e8725ae7aeacb935152
wangyuhan <1269940762@qq.com>
2026-04-22 13:48:23 +0800

    [add] add vulkane device queue info .

--- FrameGraph/Vulkan/Instance/VDevice.h
+++ FrameGraph/Vulkan/Instance/VDevice.h
@@ -13,7 +13,33 @@ namespace FrameGraph
 	// Device Queue
 	//
 
+	struct VDeviceQueueInfo
+	{
+		//变量
+		
+		//当使用 vkQueueSubmit, vkQueueWaitIdle, vkQueueBindSparse, vkQueuePresentKHR
+		mutable Mutex guard;
+		VkQueue handle = VK_NULL_HANDLE;
+		EQueueFamily familyIndex = Default;
+		VkQueueFlags familyFlags = Default;
+		float priority = 0.0f;
+		uint3 minImageTransferGranularity;
+		DebugName_t debugName;
 
+		//方法
+		VDeviceQueueInfo() {}
+		
+		VDeviceQueueInfo(VDeviceQueueInfo&& other) :
+			handle{ other.handle }, familyIndex{ other.familyIndex }, familyFlags{ other.familyFlags },
+			priority{ other.priority }, minImageTransferGranularity{ other.minImageTransferGranularity },
+			debugName{ other.debugName }
+		{
+		}
+	};
+
+	//
+	//  Vulkan Device
+	//
 
 
 }
