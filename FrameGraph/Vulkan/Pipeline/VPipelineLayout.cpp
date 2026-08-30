723fa379b0eb2e9cc0ba1ae3e5a338f095d62061
wangyuhan <1269940762@qq.com>
2026-08-30 17:36:59 +0800

    [modify] modify VPipelineLayout.cpp

--- FrameGraph/Vulkan/Pipeline/VPipelineLayout.cpp
+++ FrameGraph/Vulkan/Pipeline/VPipelineLayout.cpp
@@ -1,2 +1,29 @@
 ﻿#pragma once
 #include "VPipelineLayout.h"
+#include "VCommon.h"
+#include "../Vulkan/Instance/VDevice.h"
+
+bool FrameGraph::VPipelineLayout::Create(const VDevice& dev, VkDescriptorSetLayout emptyLayout)
+{
+	EXLOCK(_drCheck);
+	CHECK_ERR(_layout == VK_NULL_HANDLE);
+
+	VPipelineLayout::VkDescriptorSetLayouts_t vk_layouts;
+	VPipelineLayout::VkPushConstantRanges_t vk_pushConstants;
+
+	for(auto& layout : vk_layouts)
+	{
+		layout = emptyLayout;
+	}
+
+	for(auto& pushConst : vk_pushConstants)
+	{
+		pushConst = {};
+	}
+
+}
+
+void FrameGraph::VPipelineLayout::Destroy(VResourceManager&)
+{
+
+}
